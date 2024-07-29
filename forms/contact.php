6a326fca0eb1b54b4a455910a7a2413354d31ce8
Lily Nwobodo <lilynwobodo@gmail.com>
2024-07-30 00:58:30 +0100

    add: information about form functionality

--- forms/contact.php
+++ forms/contact.php
@@ -4,41 +4,54 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 
- 
-$receiving_email_address = 'lilynwobodo@gmail.com';
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Process form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-$mail = new PHPMailer(true);
+  $receiving_email_address = 'lilynwobodo@gmail.com';
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-try {
-    // Server settings
-    $mail->SMTPDebug = 0;                                      
-    $mail->isSMTP();                                           
-    $mail->Host       = 'smtp.gmail.com';                    
-    $mail->SMTPAuth   = true;                                  
-    $mail->Username   = 'lilynwobodo.com';              
-    $mail->Password   = 'jzkqshngusqhylpe';                 
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        
-    $mail->Port       = 587;                                  
+  $mail = new PHPMailer(true);
 
-    // Recipients
-    $mail->setFrom('lilynwobodo@gmail.com', 'Somto Lily');
-    $mail->addAddress('lilynwobodo@gmail.com', 'Somto Lily');   
-    $mail->addReplyTo($email, $name);
+  try {
+      // Server settings
+      $mail->SMTPDebug = 0;                                      
+      $mail->isSMTP();                                           
+      $mail->Host       = 'smtp.gmail.com';                    
+      $mail->SMTPAuth   = true;                                  
+      $mail->Username   = 'lilynwobodo.com';              
+      $mail->Password   = 'jzkqshngusqhylpe';                 
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        
+      $mail->Port       = 587;                                  
 
-    // Content
-    $mail->isHTML(true);                                       // Set email format to HTML
-    $mail->Subject = $subject;
-    $mail->Body    = $message;
-    $mail->AltBody = strip_tags($message);
+      // Recipients
+      $mail->setFrom('lilynwobodo@gmail.com', 'Somto Lily');
+      $mail->addAddress('lilynwobodo@gmail.com', 'Somto Lily');   
+      $mail->addReplyTo($email, $name);
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+      // Content
+      $mail->isHTML(true);                                   
+      $mail->Subject = $subject;
+      $mail->Body    = $message;
+      $mail->AltBody = strip_tags($message);
+
+      $mail->send();
+      echo 'Message has been sent';
+  } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+  }
+
+
+} else {
+  // Respond with a 405 Method Not Allowed error for other methods
+  http_response_code(405);
+  echo 'This is what is being sent';
 }
 
 ?>
